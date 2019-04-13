1d5dcb687a409c03683716b9fbcb72e3b02f65cf
Sergey Leschina <mail@putnik.tech>
2019-04-13 21:11:00 +0300

    Roster improvements: search field and icons

--- src/renderer/components/MainPage/Roster.vue
+++ src/renderer/components/MainPage/Roster.vue
@@ -2,17 +2,19 @@
   <div class="ui fluid vertical menu roster-wrapper">
     <div class="item">
       <div class="ui icon input">
-        <input type="text" placeholder="Search people...">
+        <input v-model="search" type="text" placeholder="Search people...">
         <i class="search icon"></i>
       </div>
     </div>
-    <a class="grey item" v-for="(contact, contactIndex) in contacts"
+    <a :class="`${contact.jid === contactJid ? 'active' : ''} item roster-item`"
+       v-for="(contact, contactIndex) in contacts"
        :item="contact"
        :index="contactIndex"
        :key="contact.jid"
        :data-jid="contact.jid"
        @click="handleClick">
-      {{ contact.name }}
+      <Avatar class="ui image roster-item-image" :username="contact.name" :size="30"/>
+      <span class="roster-item-name">{{ contact.name }}</span>
       <div class="ui label" v-if="contact.unreadCount">
         {{ contact.unreadCount > 99 ? '99+' : contact.unreadCount }}
       </div>
@@ -21,7 +23,10 @@
 </template>
 
 <script>
+import Avatar from 'vue-avatar';
+
 export default {
+  components: { Avatar },
   methods: {
     open(link) {
       this.$electron.shell.openExternal(link);
@@ -30,7 +35,15 @@ export default {
       this.$xmpp.openContact(event.target.dataset.jid);
     },
   },
+  data() {
+    return {
+      rosterSearch: '',
+    };
+  },
   computed: {
+    contactJid() {
+      return this.$store.state.xmpp.contactJid;
+    },
     contacts() {
       const contacts = {};
 
@@ -42,12 +55,33 @@ export default {
             status: 'offline',
             unreadCount: 0,
             groups: item.groups,
+            lastMessage: new Date(0),
           };
-          contacts[contact.jid] = contact;
+          const search = this.rosterSearch.toLocaleLowerCase();
+          if (search === ''
+            || contact.name.toLocaleLowerCase().match(search)
+            || contact.jid.toLocaleLowerCase().match(search)
+          ) {
+            const key = `${contact.lastMessage.toLocaleDateString()}_${contact.jid}`;
+            contacts[key] = contact;
+          }
         });
       }
 
-      return contacts;
+      const orderedContacts = {};
+      Object.keys(contacts).sort().forEach((key) => {
+        orderedContacts[contacts[key].jid] = contacts[key];
+      });
+
+      return orderedContacts;
+    },
+    search: {
+      get() {
+        return this.rosterSearch;
+      },
+      set(newValue) {
+        this.rosterSearch = newValue;
+      },
     },
   },
 };
@@ -58,5 +92,22 @@ export default {
     height: 100%;
     border-radius: 0 !important;
     border: 0 !important;
+  }
+
+  .roster-item {
+    overflow: hidden;
+  }
+
+  .roster-item-image {
+    display: inline-block !important;
+    margin-top: -6px !important;
+  }
+
+  .roster-item-name {
+    display: inline-block;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis !important;
+    max-width: 182px !important;
   }
 </style>
